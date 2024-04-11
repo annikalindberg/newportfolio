5a608cef7435ab17dd01c4e11949fab67744182d
AnnikaLindberg <annika.edit.lindberg@gmail.com>
2024-04-11 13:40:43 +0200

    fixed height in projectcards

--- lib/types.ts
+++ lib/types.ts
@@ -1,18 +1,3 @@
 import { links } from "./data";
 
 export type SectionName = (typeof links)[number]["name"];
-
-export type Project = {
-  readonly title: string;
-  readonly slug?: string; // Use this if the slug is optional
-  readonly description: string;
-    readonly imageUrl: string;
-  readonly tags: string[];
-  githubLink: string;
-liveUrl: string;
-notes: string;
-futureImprovements: string;
-
-
-  
-};
